6821f16f2f37456e70dc8086e9773559a3aadb3c
Jose Escobar <joanesco@espol.edu.ec>
2019-01-13 22:28:21 -0500

    Extraccion de datos IGEcuador con Java

--- TwitterGEO/src/twittergeo/TwitterGEO.java
+++ TwitterGEO/src/twittergeo/TwitterGEO.java
@@ -5,18 +5,20 @@
  */
 package twittergeo;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import twitter4j.Paging;
 import twitter4j.Status;
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
- * @author Fernando
+ * @author Vaca, Escobar, Palma
  */
 public class TwitterGEO {
 
@@ -26,37 +28,73 @@ public class TwitterGEO {
     public static void main(String[] args) {
         // TODO code application logic here
         setUpTwitter();
-        
-        
-        
     }
-    
-    private static void setUpTwitter(){
-        try {
-            ConfigurationBuilder cb= new ConfigurationBuilder();
-            cb.setDebugEnabled(true)
-                    .setOAuthConsumerKey("2halIYtHPBYgpDzeEyjt9rRyz")
-                    .setOAuthConsumerSecret("nFpZtU0UfxDzUZI6kxV27m1ykdwg3ARGtbeDasSpw1q5YR1NlJ")
-                    .setOAuthAccessToken("3418827076-s1ZPyH4wvD7R5dn1WPy3GpLCAOpBUHfH4fkQOMf")
-                    .setOAuthAccessTokenSecret("u32pEwjgtCCpCuwFLm4a7uazyXPw85fr2gVxey3PBXUdu");
-            
-            TwitterFactory tf=new TwitterFactory(cb.build());
-            twitter4j.Twitter twitter=tf.getInstance();
-            Paging p=new Paging();
-            p.setCount(200);
-            List<Status> status=twitter.getUserTimeline("@IGecuador",p);
-            
-            
-            int count=0;
-            for(Status st:status){
-                System.out.println(st.getUser().getName()+"-------"+st.getText()+"\n");
-                count++;
+
+    private static void setUpTwitter() {
+        final String COMMA_DELIMITER = ",";
+        final String NEW_LINE_SEPARATOR = "\n";
+        final String FILE_HEADER = "id,url,fecha,contenido,cantidadRT,cantidadFAV,";
+
+        int pageno = 1;
+        String user = "@IGecuador";
+        List<Status> statuses = new ArrayList();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        cb.setDebugEnabled(
+                true)
+                .setOAuthConsumerKey("2halIYtHPBYgpDzeEyjt9rRyz")
+                .setOAuthConsumerSecret("nFpZtU0UfxDzUZI6kxV27m1ykdwg3ARGtbeDasSpw1q5YR1NlJ")
+                .setOAuthAccessToken("3418827076-s1ZPyH4wvD7R5dn1WPy3GpLCAOpBUHfH4fkQOMf")
+                .setOAuthAccessTokenSecret("u32pEwjgtCCpCuwFLm4a7uazyXPw85fr2gVxey3PBXUdu");
+
+        Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+
+        while (true) {
+            try {
+                int size = statuses.size();
+                Paging page = new Paging(pageno++, 100);
+                statuses.addAll(twitter.getUserTimeline(user, page));
+                if (statuses.size() == size) {
+                    break;
+                }
+
+            } catch (TwitterException e) {
+                e.printStackTrace();
             }
-            System.out.println(count);
-        } catch (TwitterException ex) {
-            Logger.getLogger(TwitterGEO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter("tweetsIG.csv");
+            fileWriter.append(FILE_HEADER);
+            fileWriter.append(NEW_LINE_SEPARATOR);
+
+            for (Status st : statuses) {
+                fileWriter.append(String.valueOf(st.getId()));
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(String.valueOf(st.getDisplayTextRangeEnd()));
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(String.valueOf(st.getCreatedAt()));
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(st.getText());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(String.valueOf(st.getRetweetCount()));
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(String.valueOf(st.getFavoriteCount()));
+                fileWriter.append(NEW_LINE_SEPARATOR);
+            }
+            System.out.println("csv created!!!");
+
+        } catch (IOException e) {
+            System.out.println("Error creating csv !!!");
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                System.out.println("Error while closing fileWriter !!!");
+            }
+        }
     }
-    
+
 }
