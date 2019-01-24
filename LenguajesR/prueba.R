52f4eac6f86175feca4fef5a7a5aabff4c83e6bf
Fernando Vaca <fdvaca@espol.edu.ec>
2019-01-24 03:53:42 -0500

    cambio

--- LenguajesR/prueba.R
+++ LenguajesR/prueba.R
@@ -5,7 +5,7 @@ procesamiento <- read.csv(file="tweetsIG.csv",header=TRUE,sep="|")
 filtro<-subset(procesamiento,str_sub(fecha,start=-4)=="2018")
 filtro2<-subset(filtro,grepl("Rev.",contenido))
 filtro2$contenido<-str_sub(filtro2$contenido,start=gregexpr("2018(-|/)",filtro2$contenido))
-filtro4<-filtro2[c(56:430),c(1:2)]
+filtro4<-subset(filtro2,str_sub(contenido,start=1,end=5)=="2018/")
 filtro4$fecha<-NULL
 
 filtro5<-filtro4 %>%separate(contenido, c("datos1", "datos2","provincia"), ",")
